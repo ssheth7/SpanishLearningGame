690bd49f4b9648e0f70487f56376608d0f902275
Justin O'Boyle <justin@justinoboyle.com>
2021-11-22 16:47:13 -0500

    fix logic

--- client/components/layout/SEO.js
+++ client/components/layout/SEO.js
@@ -1,8 +1,8 @@
 import Head from "next/head"
 
 const defaults = {
-  title: "Spanish Learning Game",
-  _postTitle: "Spanish Learning Game",
+  title: "",
+  appname: "Spanish Learning Game",
   description: "A game for learning Spanish",
   keywords: ["game", "spanish", "learn spanish"],
   type: "website",
@@ -10,18 +10,17 @@ const defaults = {
 
 export default function SEO({ ...input }) {
   let meta = Object.assign({}, defaults, input)
-  if (!meta.title.includes(defaults._postTitle)) meta.title = ` ${meta.title} | ${defaults._postTitle}`
-
+  const title = meta.title == "" ? meta.appname : meta.title + " | " + meta.appname
   return (
     <Head>
-      <title>{meta.title}</title>
+      <title>{title}</title>
       <meta name="description" content={meta.description} />
       <meta name="keywords" content={meta.keywords.join(", ")} />
-      <meta property="og:title" content={meta.title} />
+      <meta property="og:title" content={title} />
       <meta property="og:description" content={meta.description} />
       <meta property="og:type" content={meta.type} />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:title" content={meta.title} />
+      <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={meta.description} />
     </Head>
   )
