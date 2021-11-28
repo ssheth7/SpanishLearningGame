abcd33600e1d22d39c6f1116e1aa78896e57ecbe
shivam <shivam@kali>
2021-11-28 09:32:51 -0500

    merge conflicts

---
+++ client/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Nav, Navbar, Container, NavDropdown } from 'react-bootstrap';
+
+export default function NavBar() {
+  return (
+    <>
+      <Nav>navbar</Nav>
+    </>
+    )
+}
