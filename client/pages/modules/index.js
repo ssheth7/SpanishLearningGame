2c27f980b0ecc04223d3fb26b133809324136506
Justin O'Boyle <justin@justinoboyle.com>
2021-12-05 16:08:20 -0500

    basic modules page

--- client/pages/modules/index.js
+++ client/pages/modules/index.js
@@ -2,30 +2,35 @@ import AppLayout from "../../components/layout/AppLayout"
 
 import { getAllLevels } from "../../utils/query/material"
 
+import styles from "./index.module.css"
+
+import AppContainer from "../../components/common/AppContainer"
+
 // TODO make prettier
 export default function ModulesPage({ levels }) {
-  console.log({ levels })
   return (
     <AppLayout activePage="/modules">
-      <h1>Available modules</h1>
+      <AppContainer>
+        <h1>Available modules</h1>
 
-      {levels ? (
-        levels.map((level) => (
-          <div key={level.id}>
-            <h2>{level.title}</h2>
-            <p>{level.description}</p>
-            <ul>
-              {level.modules.map((module) => (
-                <li key={module.id}>
-                  <a href={`/modules/${level.id}/${module.id}`}>{module.title}</a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))
-      ) : (
-        <p>Just a sec, we're getting things ready...</p>
-      )}
+        {levels ? (
+          levels.map((level) => (
+            <div className={styles.module} key={level.id}>
+              <h2>{level.title}</h2>
+              <p>{level.description}</p>
+              <ul>
+                {level.modules.map((module) => (
+                  <li key={module.id}>
+                    <a href={`/modules/${level.id}/${module.id}`}>{module.title}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))
+        ) : (
+          <p>Just a sec, we're getting things ready...</p>
+        )}
+      </AppContainer>
     </AppLayout>
   )
 }
