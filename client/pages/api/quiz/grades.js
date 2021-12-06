e47111cc0d58deb5807d3ffc826bf16fb6bcd2f6
Justin O'Boyle <justin@justinoboyle.com>
2021-12-06 13:49:33 -0500

    Grade query

--- client/pages/api/quiz/grades.js
+++ client/pages/api/quiz/grades.js
@@ -7,14 +7,44 @@ const handler = async function (req, res) {
       error: "Method not allowed",
     })
 
-//   const { user } = req
+  if (!req.user) {
+    return res.status(401).json({
+      error: "Unauthorized",
+    })
+  }
 
-//   const rel = new ModuleGradeRelationship({ user: user, grade, module_id, attemptDate: new Date() })
-//   await rel.save()
+  const { user } = req
 
-//   console.log({ rel })
+  const gradeRelationships = await ModuleGradeRelationship.find({
+    user: user,
+  })
+
+  let rel = {}
+
+  for (let attempt of gradeRelationships) {
+    const { module_id, grade, attemptDate } = attempt
+    if (!rel[module_id]) {
+      rel[module_id] = {
+        grade,
+        attempts: [],
+      }
+    }
+    if (grade > rel[module_id].grade) {
+      rel[module_id].grade = grade
+    }
+    rel[module_id].attempts.push({
+      grade,
+      attemptDate,
+    })
+  }
+
+  //   const rel = new ModuleGradeRelationship({ user: user, grade, module_id, attemptDate: new Date() })
+  //   await rel.save()
+
+  //   console.log({ rel })
 
   return res.status(200).json({
+    grades: rel,
     success: true,
   })
 }
