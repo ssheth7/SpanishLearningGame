5c4d99f71f21aaa1241fb02f6eab08b782077520
Justin O'Boyle <justin@justinoboyle.com>
2021-12-06 10:48:09 -0500

    Add auth, needs styling

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,13 @@
 import "../styles/globals.css"
 
+import { SessionProvider } from "../utils/hooks/auth"
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 export default MyApp
