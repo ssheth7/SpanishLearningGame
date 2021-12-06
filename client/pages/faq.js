6076d54f0a705ed3194ebd4d92cf3a13825310db
shivam <shivam@kali>
2021-12-06 16:25:15 -0500

    Updated navbar and added faq questions

--- client/pages/faq.js
+++ client/pages/faq.js
@@ -9,13 +9,31 @@ import CardLayout from "../components/common/CardLayout"
 // TODO add questions here
 const questions = [
   {
-    question: "What is a question?",
-    answer: "This is an answer.",
+    question: "What is this website?",
+    answer: "This website gives people an opportunity to learn Spanish on a quick and easy to use interface.",
   },
   {
-    question: "What is another question?",
-    answer: "This is another answer.",
+    question: "How do I use this website?",
+    answer: "First, the home page presents different difficulties for you to start with. Read through the different options and pick what works for you! In each level, \
+    there are modules that teach you different topics. Each module contains a preliminary exam, learning flashcards, and a quiz. The accompanying quiz tests you on the material taught\
+    taught in the module. You need an 80% to successfully complete a quiz.",
   },
+  {
+    question: "Where can I send feedback?",
+    answer: "We love hearing from our users! You can find the feedback form by clicking the \"Feedback\" tab on the navigation bar on top."
+  },
+  {
+    question: "Do I have to start at the beginner difficulty?",
+    answer: "No, we provide users of different skill levels to start wherever they see fit!",
+  },
+  {
+    question: "How can I create an account?",
+    answer: "You can create an account by clicking the \"Create an Account\" tab on the navigation bar on top."
+  },
+  {
+    question: "How do I see the flashcard term definitions?",
+    answer: "You can flip over the flashcards and see the definitions by clicking on them!"
+  }
 ]
 
 export default function FAQ() {
@@ -31,6 +49,7 @@ export default function FAQ() {
           {questions.map(({ question, answer }) => (
             <CardLayout key={question}>
               <h3>{question}</h3>
+              <br></br>
               <p>{answer}</p>
             </CardLayout>
           ))}
